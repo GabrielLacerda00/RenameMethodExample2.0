89dfa1c3aa1f8e2219e1b56cdf85210816fe84e5
GabrielLacerda00 <gabriel.lacerd.gr@gmail.com>
2023-07-08 23:42:07 -0300

    metodo soma

---
+++ calc.java
@@ -0,0 +1,5 @@
+public class calc {
+  public static double soma1(double a, double b) {
+      return a + b;
+  }
+}
---
+++ calculadora.java
@@ -0,0 +1,11 @@
+
+public class calculadora extends calc{
+  public static void main(String[] args) {
+     soma0();
+  }
+
+  public static double soma0() {
+    return soma1(0, 0);
+  }
+
+}
